c7c2fd07e948c21dc69be87e1bd9b4073153afb9
abdullahchand <abdullah.chand96@gmail.com>
2023-02-14 13:24:08 +0500

    Fixed mask channel issue

--- utils/mask_utils.py
+++ utils/mask_utils.py
@@ -1,6 +1,10 @@
 import cv2
 
 def convert_to_1D(mask):
-    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-    resized_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    resized_mask = mask
+    if len(mask.shape) > 2:
+        h,w,c = mask.shape
+        if c>1:
+            gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+            resized_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     return resized_mask
